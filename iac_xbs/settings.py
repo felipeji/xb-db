a55a985a0a03971c00b2968acf2c337f35cc0644
Felipe Jimenez-Ibarra <felipejimenezibarra@gmail.com>
2024-03-13 12:40:55 +1100

    PostgreSQL database implemented

--- iac_xbs/settings.py
+++ iac_xbs/settings.py
@@ -81,12 +81,19 @@ WSGI_APPLICATION = 'iac_xbs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'xbdb',
+        'USER': 'iac_xbs',
+        'PASSWORD': 'binaries',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
