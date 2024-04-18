8dd18408549ee77d56b9204b01ceec6a50f5c237
Felipe Jimenez-Ibarra <felipejimenezibarra@gmail.com>
2024-04-18 19:58:24 +1000

    Lambda 0 selection in dropdown menus.

--- selection/views.py
+++ selection/views.py
@@ -17,8 +17,6 @@ from astropy.constants import c
 @csrf_exempt
 def process_selection(request):
     
-    # Data base dir
-
     if request.method == 'POST':
         # Retrieve the list of selected row IDs from the POST parameters
         selected_ids = np.int_(json.loads(request.POST['selected_rows']))
@@ -26,11 +24,8 @@ def process_selection(request):
         # Filter Selected Spectra
         spect_objects = Spect.objects.filter(spect_id__in=selected_ids)
 
-
-        # Convert the Plotly figure to HTML
+        # Priduce plots
         wave_spect_plot, vel_spect_plot  = spect_plot(spect_objects)
-
-
 
 
         # Pass the plot HTML and selected_spects to the template
@@ -46,74 +41,6 @@ def read_spec(file, slot):
 
 
 
-# def spect_plot(spect_objects):
-#     """
-#     Create a Plotly figure for the given spectra.
-
-#     Args:
-#     - spect_objects: QuerySet containing the selected spectra objects
-
-#     Returns:
-#     - Plotly figure object
-#     """
-
-#     traces = []
-
-    
-#     # Read .mol spectra data for each selected spectrum and store in wave_data and flux_data determining the wavelength range of the whole set
-#     w_range = [np.inf, -np.inf]
-
-#     for selected_spect in spect_objects:
-#         file = str(selected_spect.file)
-#         file_path = os.path.join(settings.BASE_DIR, 'dotmol', 'database', file)
-            
-#         slot = selected_spect.slot
-#         wave, flux = read_spec(file_path, slot)
-
-#         # Check max and min
-#         w_range[0] = min(w_range[0],min(wave))
-#         w_range[1] = max(w_range[1],max(wave))
-
-#         name = 'File: ' + file + '    Slot: ' + str(slot)
-
-#         x_axis = 'vel'
-#         x_axis = 'wave'
-
-#         if x_axis is 'wave':
-#             trace = go.Scatter(x=wave, y=flux, mode='lines', line_shape='hvh', name=name)
-
-#         if x_axis is 'vel':
-#             # Velocity space
-#             lambda_0 = 6065
-#             lambda_0 = lambda_0 * u.AA
-#             vel = c * ((wave*u.AA - lambda_0) / lambda_0)
-
-#             trace = go.Scatter(x=vel, y=flux, mode='lines', line_shape='hvh', name=name)
-
-#         traces.append(trace)
-
-
-#     # Create a Plotly layout
-#     layout = go.Layout( xaxis=dict(title=r'Wavelength (Å)'), yaxis=dict(title='Intensity'))
-
-#     # Create a Plotly figure
-#     fig = go.Figure(data=traces, layout=layout)
-
-#     # Call spec_lines function to add vertical line
-#     # List of transitions
-#     transitions = [
-#         [H.balmer, "Balmer",'black'],
-#         [H.paschen, "Pashen", 'black'],
-#         [He.I, "HeI", 'black'],
-#         [He.II, "HeII", 'black'],
-#         [sky.sky, "Sky lines", '#3498DB'],
-
-#     ]
-#     spec_line(fig, w_range,transitions)
-
-
-
-#     return fig.to_html(full_html=False)
 
 def spect_plot(spect_objects):
     traces_wave = []
@@ -137,35 +64,55 @@ def spect_plot(spect_objects):
         traces_wave.append(trace_wave)
 
         # Velocity plot
-        lambda_0 = 6065 * u.AA
-        vel = (c * ((wave * u.AA - lambda_0) / lambda_0)).to(u.km/u.s).value
-        trace_vel = go.Scatter(x=vel, y=flux, mode='lines', line_shape='hvh', name=name)
+        trace_vel = go.Scatter(x=wave, y=flux, mode='lines', line_shape='hvh', name=name)
         traces_vel.append(trace_vel)
 
+
     # Layouts
-    layout_wave = go.Layout(xaxis=dict(title='Wavelength (Å)'), yaxis=dict(title='Intensity'))
-    layout_vel = go.Layout(xaxis=dict(title='Velocity (km/s)'), yaxis=dict(title='Intensity'))
+    layout_wave = go.Layout(xaxis=dict(title='Wavelength (Å)'), yaxis=dict(title='Intensity'), )
+    layout_vel = go.Layout(xaxis=dict(title='Velocity (km/s)', ), yaxis=dict(title='Intensity'),)
+
+
 
     # Figures
     fig_wave = go.Figure(data=traces_wave, layout=layout_wave)
     fig_vel = go.Figure(data=traces_vel, layout=layout_vel)
 
+    
     # Plot reference lines in wavelenght representation
     # List of transitions
     transitions = [
-        [H.balmer, "Balmer",'black'],
-        [H.paschen, "Pashen", 'black'],
-        [He.I, "HeI", 'black'],
-        [He.II, "HeII", 'black'],
-        [sky.sky, "Sky lines", '#3498DB'],
+        [range_filter(H.balmer, w_range), "Balmer",'black'],
+        [range_filter(H.paschen, w_range), "Pashen", 'black'],
+        [range_filter(He.I, w_range), "HeI", 'black'],
+        [range_filter(He.II, w_range), "HeII", 'black'],
+        [range_filter(sky.sky, w_range), "Sky lines", '#3498DB'],
     ]
 
-    spec_line(fig_wave, w_range, transitions) 
+
+    lambda_buttons(fig_vel, transitions)
+
+    fig_vel.update_layout( width=1200, height=700,
+    xaxis=dict( 
+        rangeslider=dict( 
+            visible=True
+        ), 
+        ) 
+    ) 
+
+    fig_wave.update_layout( width=1200, height=600,
+    ) 
+
+
+    spec_line(fig_wave, transitions) 
+
+   
+
     return fig_wave.to_html(full_html=False), fig_vel.to_html(full_html=False)
 
 
 
-def spec_line(figure, w_range, transitions):
+def spec_line(figure, transitions):
     buttons = [
         dict(
             label="None",
@@ -184,23 +131,22 @@ def spec_line(figure, w_range, transitions):
         for line in line_list:
             label = line[0]
             wavelength = line[1]
-            if w_range[0] <= wavelength <= w_range[1]:
-                lines.append(
-                    dict(
-                        type='line',
-                        x0=wavelength,
-                        y0=0,
-                        x1=wavelength,
-                        y1=1,
-                        line=dict(color=color, width=2, dash='dash'),
-                        yref='paper',
-                        label=dict(text=label,
-                                   textposition="end",
-                                   textangle="0",
-                                   font = dict(size=15, color=color)
-                                   ),
-                    )
+            lines.append(
+                dict(
+                    type='line',
+                    x0=wavelength,
+                    y0=0,
+                    x1=wavelength,
+                    y1=1,
+                    line=dict(color=color, width=2, dash='dash'),
+                    yref='paper',
+                    label=dict(text=label,
+                                textposition="end",
+                                textangle="0",
+                                font = dict(size=15, color=color)
+                                ),
                 )
+            )
 
         button = dict(
             label=button_label,
@@ -249,3 +195,55 @@ def spec_line(figure, w_range, transitions):
 
 
 
+
+def lambda_buttons(figure, transitions):
+    buttons = []
+
+    for transition in transitions:
+        line_list = transition[0]
+
+        for line in line_list:
+            label = line[0]
+            wavelength = line[1]
+
+            lambda_0 = wavelength * u.AA
+
+            # TODO IDK WHy limit to 3 but otherwise it will not work
+            args  = [{"x": [(c * ((trace.x * u.AA - lambda_0) / lambda_0)).to(u.km/u.s).value ]} for trace in figure.data[0:3]] 
+            
+            button = dict(
+                label= label + f" - {wavelength} Å",
+                method="update",
+                args=args  
+            )
+            buttons.append(button)
+
+        # Add an initial label to the dropdown menu
+        initial_label = "Select λ₀"
+        buttons.insert(0, dict(label=initial_label, method="skip", args=[{"visible": False, }]))
+
+
+        figure.update_layout(
+            updatemenus=[
+                dict(
+                    type="dropdown",
+                    buttons=buttons,
+                    direction="down",  # Align the buttons horizontally
+                    x=0.3,  # Position the buttons in the center
+                    y=1.25,  # Adjust the vertical position of the buttons
+                    showactive=True,
+                    bgcolor='#E5E8E8',
+                    borderwidth=0,
+                    font=dict(size=14, color="#566573", family="verdana"),
+
+                )
+            ]
+        )
+
+def range_filter(line_list, w_range):
+    filtered = []
+    for line in line_list:
+        wavelength = line[1]
+        if w_range[0] <= wavelength <= w_range[1]:
+            filtered.append(line)
+    return filtered
