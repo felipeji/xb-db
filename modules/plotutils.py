f4d38e9a85347464783f9169f794e1f970f82604
Felipe Jimenez-Ibarra <felipejimenezibarra@gmail.com>
2024-04-22 22:11:08 +1000

    Bug fixed in plots.

--- modules/plotutils.py
+++ modules/plotutils.py
@@ -210,7 +210,7 @@ def spect_plot(spect_objects, wavelength):
 
     fig_vel.update_layout(
         margin=dict(l=20, r=20, t=10, b=0),
-        modebar=dict(remove=["zoomout", "zoomin",],orientation='v',visible=True,),
+        modebar=dict(remove=["zoomout", "zoomin",],orientation='v'),
         width=610,
         height=410,
         xaxis=dict(
